3c96fc8760d08214eb29ebd7ed3872cc31fd4bf4
Tyler Taylor <vikingtyty@gmail.com>
2019-02-01 09:58:01 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`))
