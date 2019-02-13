8eea1d58eea76f1865df8be48afc6ac9599a4fe6
Tyler Taylor <vikingtyty@gmail.com>
2019-02-13 11:47:26 -0600

    Scroll to bottom if user is near the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 let socket = io()
 
+function scrollToBottom() {
+  // Selectors
+  let messages = $('#messages')
+  let newMessage = messages.children('li:last-child')
+
+  // Heights
+  let clientHeight = messages.prop('clientHeight')
+  let scrollTop = messages.prop('scrollTop')
+  let scrollHeight = messages.prop('scrollHeight')
+  let newMessageHeight = newMessage.innerHeight()
+  let lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server')
 })
@@ -19,6 +36,7 @@ socket.on('newMessage', function (message) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -36,6 +54,7 @@ socket.on('newLocationMessage', function (message) {
   })
 
   $('#messages').append(html)
+  scrollToBottom()
 })
 
 $('#message-form').on('submit', function (e) {
